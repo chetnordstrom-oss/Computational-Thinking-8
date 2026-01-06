16919640c772058e6380941a8e1176bc4ee037c7
chetnordstrom-oss <chetnordstrom@seattleacademy.org>
2026-01-06 22:22:56 +0000

    hi

---
+++ Projects/project2.py
@@ -0,0 +1,31 @@
+winter_points = 1
+summer_points=1
+spring_points=1
+
+answer1 = input ("do you perfer summer or spring, B summer, or C both?")
+if answer1 == "A":
+    summer_points += 1
+elif answer1 == "B":
+    summer_points +=1
+spring_points +=1
+
+
+
+
+awnser2 = input ("do you like sledding or swimming?")
+if awnser2 == "sledding":
+    winter_points += 1
+elif awnser2 == "swimming":
+    summer_points+=1
+
+
+awnser3 = input (" do you like snow or sun?")
+if awnser3 == "snow":
+winter_points +=1
+sun_points+ +=1
+("do you like hot chocalete or ice cream")
+winter_points +=1
+summer_points +=1
+("do you like snow ball fights and trampaling. or watter balloon fights?")
+winter_points +=1 
+summer_points +=1
