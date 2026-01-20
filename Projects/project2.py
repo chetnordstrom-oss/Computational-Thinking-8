f83dda888ba2f75b6eb0b4ae1695e2758e71dbe2
chetnordstrom-oss <chetnordstrom@seattleacademy.org>
2026-01-20 17:15:34 +0000

    hi

--- Projects/project2.py
+++ Projects/project2.py
@@ -1,12 +1,12 @@
 winter_points = 1
-summer_points=1
-spring_points=1
+summer_points = 1
+spring_points = 1
 
 answer1 = input ("do you prefer summer or spring?")
 if answer1 == "summer":
     summer_points += 1
 elif answer1 == "spring":
-    spring_points +=1
+    spring_points += 1
 
 
 
@@ -15,31 +15,31 @@ answer2 = input ("do you like sledding or swimming?")
 if answer2 == "sledding":
     winter_points += 1
 elif answer2 == "swimming":
-    summer_points+=1
+    summer_points+= 1
 
 
 answer3 = input (" do you like snow or sun?")
 if answer3 == "snow" or answer3 == "Snow":
-    winter_points +=1
+    winter_points += 1
 elif answer3 == "sun":
-    summer_points +=1
+    summer_points += 1
 
 answer4 = input("do you like hot chocolate or ice cream?")
 if answer4 == "hot chocolate":
-    winter_points +=1
+    winter_points += 1
 elif answer4 == "ice cream":
-    summer_points +=1
+    summer_points += 1
 
-answer5 = input("do you like ice or snow")
-if answer5 == "ice":
-    winter_points +=1
+answer5 = input("do you like ice or snow?")
+if answer5 == "ice" or answer5 == "B":
+   winter_points += 1
 elif answer5 == "snow":
-    winter_points+=2
+    winter_points += 2
 
 
-if winter_points > summer_points and winter_points > spring_points:
+if winter_points >= summer_points and winter_points >= spring_points:
     print("you are a winter person")
-if summer_points > winter_points and summer_points > spring_points:
+elif summer_points > winter_points and summer_points > spring_points:
     print("you are a summer person")
-if spring_points > summer_points:
+elif spring_points > summer_points:
     print("you are a spring person")
